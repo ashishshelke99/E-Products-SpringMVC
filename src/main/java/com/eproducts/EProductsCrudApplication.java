55148e8d09da7215e21a10f6f2fba6338a7e7c07
ashishshelke99 <anshelke2203@gmail.com>
2021-01-06 17:17:39 +0530

    Initial Commit

---
+++ src/main/java/com/eproducts/EProductsCrudApplication.java
@@ -0,0 +1,13 @@
+package com.eproducts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EProductsCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EProductsCrudApplication.class, args);
+	}
+
+}
